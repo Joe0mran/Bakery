ce7aaf02e3fe9387330063d52e6ffd82ecebaa2e
FaresGawwad <faresellbbad@gmail.com>
2025-05-01 20:38:35 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,3 @@
-function themeToggle() {
-  const body = document.body;
-  const themeToggleButton = document.getElementById("theme-toggle-button");
-  const isDarkMode = body.classList.toggle("dark-mode");
-
-  if (isDarkMode) {
-    themeToggleButton.innerHTML = '<img src="brightness.png" alt=""/>';
-  } else {
-    themeToggleButton.innerHTML = '<img src="moon-stars.png" alt="">';
-  }
-}
+loadTheme();
+const themebutton = document.getElementById('theme');
+themebutton.addEventListener('click', theme);

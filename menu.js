60168baa84a06f060182708877a7ae1088638317
Sarahussi <sarahussien464@gmail.com>
2025-05-08 00:40:08 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,3 +2,23 @@ loadTheme();
 const themebutton = document.getElementById('theme');
 themebutton.addEventListener('click', theme);
 
+const images = [
+  'back.jpg',
+  'cc.jpg',
+  'coffe.jpg',
+  'croisson.jpg'
+];
+
+let current = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+  const bgDiv = document.getElementById('background');
+
+  function changeBackground() {
+    bgDiv.style.backgroundImage = `url('${images[current]}')`;
+    current = (current + 1) % images.length;
+  }
+
+  changeBackground();
+  setInterval(changeBackground, 2000);
+});

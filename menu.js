d1c52c5e14988fa9d48e89ec1af05bf735c1f2b0
Sarahussi <sarahussien464@gmail.com>
2025-05-08 00:43:22 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ const themebutton = document.getElementById('theme');
 themebutton.addEventListener('click', theme);
 
 const images = [
+  '670c40b0-3e3e-405f-824d-19d840027923.jpg',
   'back.jpg',
   'cc.jpg',
   'coffe.jpg',

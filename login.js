14c8ae131b82b1e5557557f28a104acf595460c5
FaresGawwad <faresellbbad@gmail.com>
2025-05-08 23:46:09 +0300

    Update login.js

--- login.js
+++ login.js
@@ -6,43 +6,32 @@ const email_input = document.getElementById('email_input');
 const password_input = document.getElementById('password_input');
 
 form.addEventListener('submit', (e) => {
-    console.log('Form submitted!');
-  email_input.parentElement.classList.remove('incorrect');
-  password_input.parentElement.classList.remove('incorrect');
-
- 
-  const errors = getLoginFormErrors(email_input.value, password_input.value);
-
+  let errors = [];
+  if (email_input){
+    errors = getLoginFormErrors(email.value,password.value);
+  }
   if (errors.length > 0) {
     e.preventDefault();
-    displayErrors(errors); 
   }
-});
-
+})
 function getLoginFormErrors(email, password) {
   let errors = [];
-
-  if (email === '' || email == null) {
-    errors.push('Email is required');
-    email_input.parentElement.classList.add('incorrect');
+      
+    if (email === '' || email == null) {
+      errors.push('Email is required');
+      email.parentElement.classList.add('incorrect');
+    }
+  
+    if (password === '' || password == null || password.length < 8) {
+      errors.push('Password is required');
+      password.parentElement.classList.add('incorrect');
+    } 
+    return errors;
   }
 
-  if (password === '' || password == null) {
-    errors.push('Password is required');
-    password_input.parentElement.classList.add('incorrect');
+    if (password === '' || password == null) {
+      errors.push('Password is required');
+      password.parentElement.classList.add('incorrect');
+    } 
+    return errors;
   }
-
-  return errors;
-}
-
-function displayErrors(errors) {
-  const errorContainer = document.getElementById('error-messages');
-  errorContainer.innerHTML = ''; 
-
-  errors.forEach((error) => {
-    const errorElement = document.createElement('p');
-    errorElement.textContent = error;
-    errorElement.style.color = 'red';
-    errorContainer.appendChild(errorElement);
-  });
-}

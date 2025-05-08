abb6850e680507cac4084a73dc65788be0c9554a
Sarahussi <sarahussien464@gmail.com>
2025-05-08 20:22:52 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,4 +2,18 @@ loadTheme();
 const themebutton = document.getElementById('theme');
 themebutton.addEventListener('click', theme);
 
+document.addEventListener("DOMContentLoaded",function() {
+    const back =[ "cc.jpg" ,"coffe.jpg","back.jpg"] ;
+
+    const img= document.getElementById("img");
+
+    let index=0;
+    function changeimage(){
+    index= (index+1)% back.length;
+
+    img.src=back[index];
+}
+    setInterval(changeimage,2000);
+})
+
 
